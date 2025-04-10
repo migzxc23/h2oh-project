a6c30dc86251c35ec91a27c32da14d399525393f
migs.recido <Miguel.Recido@ewageas.com.ph>
2025-04-10 22:11:57 +0800

    [H2oh-1] - update sql scripts to have admin credentials on the fly

--- project/src/main/resources/user_db.sql
+++ project/src/main/resources/user_db.sql
@@ -5,3 +5,6 @@ CREATE TABLE IF NOT EXISTS USER_DB (
     email VARCHAR(255) NOT NULL,
     role VARCHAR(255) NOT NULL
 );
+
+INSERT INTO USER_DB (username, password, email, role)
+VALUES ('admin', '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8', 'admin@h2oh.com', 'ADMIN');
